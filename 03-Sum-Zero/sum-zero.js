1ae0324bbe9f166da1c1940104d0ac8a27e24f94
Eledaren <eze.gonz.la.95@gmail.com>
2022-05-12 19:58:15 -0400

    Resolved exercise

--- 03-Sum-Zero/sum-zero.js
+++ 03-Sum-Zero/sum-zero.js
@@ -8,4 +8,20 @@
  * @returns {Array || undefined} - deberia retornar un array o undefined.
  */
 
-function sumZero(arr) {}
+function sumZero(arr) {
+    let arr2 = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j--) {
+            if (arr[i] + arr[j] === 0) {
+                arr2.push(arr[i]);
+                arr2.push(arr[j]);
+                return arr2;
+            } else {
+                return undefined;
+            }
+        }
+    }
+}
+
+sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 4]);
+sumZero([-3, -2, -1, 0, 1, 2]);
